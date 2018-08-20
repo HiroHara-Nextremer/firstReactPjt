981b662eecc19a08c28bcb9ffd149fc96ee93a93
Hiroaki Hara <hiroaki.hara@nextremer.com>
2018-08-20 10:11:07 +0900

    add file

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -3,20 +3,24 @@ import React, {Component} from 'react';
 class Counter extends Component {
     state = {
         count: 0,
-        address: {}
     };
+
+    styles = {
+        fontSize: 10,
+        fontWeight: 'bold'
+    }
     render() {
         return (
             <div>
-                <span>{this.formatCount()}</span>
-                <button>click here</button>
+                <span style={this.styles} className="badge badge-primary m-2" >{this.formatCount()}</span>
+                <button className="btn btn-secondary btn-sm" >click here</button>
             </div>
         );
     }
 
     formatCount() {
         const {count} = this.state;
-        return count === 0 ? <h1>ZERO</h1> : count;
+        return count === 0 ? <h3>ZERO</h3>   : count;
     }
 }
 
